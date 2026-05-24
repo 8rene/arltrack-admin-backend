c6567fe966f3f5a066495cc0601b1af02eafa133
8rene <irene.borja@dorsu.edu.ph>
2026-05-25 00:24:25 +0800

    add firebase js update

--- config/firebaseConnection/firebase.js
+++ config/firebaseConnection/firebase.js
@@ -1,8 +1,8 @@
 import admin from "firebase-admin";
-import { createRequire } from "module";
 
-const require = createRequire(import.meta.url);
-const serviceAccount = require("./firebase.key.json");
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT.replace(/\\n/g, '\n')
+);
 
 if (!admin.apps.length) {
   admin.initializeApp({
@@ -11,5 +11,3 @@ if (!admin.apps.length) {
 }
 
 export const db = admin.firestore();
-
-
